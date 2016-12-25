c4b8f139455496190b55430b65bd47e06860b76d
Andrew Stiles <andrewbstiles@gmail.com>
2016-12-25 01:52:59 -0600

    Use skeleton's container class

--- webapp/src/content.ts
+++ webapp/src/content.ts
@@ -1,1 +1,5 @@
-export default "It works from content.ts.";
+const content = "It works from content.ts.";
+var container = document.createElement('div');
+container.className = 'container';
+container.innerHTML = content;
+export default container;
--- webapp/src/entry.ts
+++ webapp/src/entry.ts
@@ -2,4 +2,5 @@ import "skeleton-css/css/normalize.css"
 import "skeleton-css/css/skeleton.css"
 import "./style.css";
 import content from "./content.ts"
-document.write(content);
+document.addEventListener("DOMContentLoaded",
+    event => document.body.appendChild(content));
