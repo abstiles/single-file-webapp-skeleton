26de81cb596bd17102b8b0575a47408e08f72a59
Andrew Stiles <andrewbstiles@gmail.com>
2016-12-24 22:04:49 -0600

    Inject JS into HTML head
    This is pretty much just an aesthetic change for the expected use.

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
+            inject: 'head',
             inlineSource: '.(js|css)$'
         }),
         new HtmlWebpackInlineSourcePlugin(),
